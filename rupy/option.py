4079c8a0f2711d1be8ebe386f7eff38ae9d184de
Endre Bjørgo <endre.bjoergo@tryg.no>
2025-01-21 10:22:32 +0100

    Add simplest Result and Option methods

--- rupy/option.py
+++ rupy/option.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Callable
+from typing import TypeVar, Callable, Self
 
 from .enum import enum, EnumCase
 from .common import unreachable
@@ -6,51 +6,164 @@ from .common import unreachable
 
 T = TypeVar("T")
 E = TypeVar("E")
+U = TypeVar("U")
 
 @enum
 class Option[T]:
     Some = EnumCase(value=T)
     Not = EnumCase()
 
-    """
-    def ok_or(self, err: E) -> Result[T, E]:
+    def is_some(self) -> bool:
+        return self == Option.Some(_)
+
+    def is_some_and(self, f: Callable[[T], bool]) -> bool:
         match self:
-            case Option.Some(value): return Result.Ok(value)
-            case Option.Not(): return Result.Err(err)
-    """
+            case Option.Some(value): return f(value)
+            case Option.Not(_): return False
+            case _: unreachable()
+
+    def is_none(self) -> bool:
+        return self == Option.Not(_)
+    
+    def is_none_or(self, f: Callable[[T], bool]) -> bool:
+        match self:
+            case Option.Some(value): return f(value)
+            case Option.Not(_): return True
+            case _: unreachable()
+
+    # def as_ref(self): pass
+    # def as_mut(self): pass
+    # def as_pin_ref(self): pass
+    # def as_pin_mut(self): pass
+    # def as_pin(self): pass
+    # def as_mut_slice(self): pass
+
+    def expect(self, msg: str) -> T:
+        match self:
+            case Option.Some(value): return value
+            case Option.Not(_): raise Exception(msg)
+            case _: unreachable()
 
     def unwrap(self) -> T:
         match self:
             case Option.Some(value): return value
-            case Option.Not(): raise Exception("called `Option.unwrap()` on a `Not` value")
+            case Option.Not(_): raise Exception("called `Option.unwrap()` on a `Not` value")
             case _: unreachable()
 
     def unwrap_or(self, default: T) -> T:
         match self:
             case Option.Some(value): return value
-            case Option.Not(): return default 
+            case Option.Not(_): return default 
+            case _: unreachable()
+
+    def unwrap_or_else(self, f: Callable[[...], T]) -> T:
+        match self:
+            case Option.Some(value): return value
+            case Option.Not(_): return f() 
             case _: unreachable()
 
     """
     def unwrap_or_default(self) -> T:
         match self:
             case Option.Some(value): return value
-            case Option.Not(): return type(T)()
+            case Option.Not(_): return type(T)()
             case _: unreachable()
     """
 
-    def unwrap_or_else(self, f: Callable[[...], T]) -> T:
+    # def unwrap_unchecked(self): pass
+
+    def map(self, f: Callable[[T], U]) -> Self:
         match self:
-            case Option.Some(value): return value
-            case Option.Not(): return f() 
+            case Option.Some(value): return Option.Some(f(value))
+            case Option.Not(_): return self
             case _: unreachable()
 
-def option_convert(f: Callable[[...], T]) -> Callable[[...], Option[T]]:
-    def wrapper(*args, **kwargs) -> Option[T]:
-        result = f(*args, **kwargs)
-        if result is None:
-            return Option.Not()
-        else:
-            return Option.Some(result)
+    # def inspect(self): pass
 
-    return wrapper
+    def map_or(self, default: U, f: Callable[[T], U]) -> U:
+        match self:
+            case Option.Some(value): return f(value)
+            case Option.Not(_): return default 
+            case _: unreachable()
+
+    # def map_or_else(self): pass
+
+    """
+    def ok_or(self, err: E) -> Result[T, E]:
+        match self:
+            case Option.Some(value): return Result.Ok(value)
+            case Option.Not(_): return Result.Err(err)
+            case _: unreachable()
+    """
+
+    # def ok_or_else(self): pass
+    # def as_deref(self): pass
+    # def as_deref_mut(self): pass
+    # def iter(self): pass
+    # def iter_mut(self): pass
+
+    def andd(self, optb: Self) -> Self:
+        match self:
+            case Option.Some(_): return optb
+            case Option.Not(_): return self
+            case _: unreachable()
+
+    def and_then(self, f: Callable[[T], Self]) -> Self:
+        match self:
+            case Option.Some(value): return f(value)
+            case Option.Not(_): return self
+            case _: unreachable()
+
+    def filter(self, f: Callable[[T], bool]) -> Self:
+        match self:
+            case Option.Some(value): return self if f(value) else Option.Not(_)
+            case Option.Not(_): return self
+            case _: unreachable()
+
+    def orr(self, optb: Self) -> Self:
+        match self:
+            case Option.Some(value): return self 
+            case Option.Not(_): return optb
+            case _: unreachable()
+
+    def or_else(self, f: Callable[[...], Self]) -> Self:
+        match self:
+            case Option.Some(value): return self
+            case Option.Not(_): return f()
+            case _: unreachable()
+
+
+    def xor(self, optb: Self) -> Self:
+        match self:
+            case Option.Some(_):
+                match optb:
+                    case Option.Some(_): return Option.Not()
+                    case Option.Not(_): return self
+                    case _: unreachable()
+            case Option.Not(_): 
+                match optb:
+                    case Option.Some(_): return optb
+                    case Option.Not(_): return self
+                    case _: unreachable()
+            case _: unreachable()
+
+
+    # def insert(self): pass
+    # def get_or_insert(self): pass
+    # def get_or_insert_default(self): pass
+    # def get_or_insert_with(self): pass
+    # def take(self): pass
+    # def take_if(self): pass
+    # def replace(self): pass
+    # def zip(self): pass
+    # def zip_with(self): pass
+
+    def convert(f: Callable[[...], T]) -> Callable[[...], Self]:
+        def wrapper(*args, **kwargs) -> Option[T]:
+            result = f(*args, **kwargs)
+            if result is None:
+                return Option.Not()
+            else:
+                return Option.Some(result)
+
+        return wrapper
--- rupy/result.py
+++ rupy/result.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Callable
+from typing import TypeVar, Callable, Self
 
 from .enum import enum, EnumCase
 from .option import Option
@@ -6,22 +6,79 @@ from .common import unreachable
 
 T = TypeVar("T")
 E = TypeVar("E")
+U = TypeVar("U")
+F = TypeVar("F")
 
 @enum
 class Result[T, E]:
     Ok = EnumCase(value=T)
     Err = EnumCase(err=E)
 
+    def is_ok(self) -> bool:
+        return self == Result.Ok(_)
+
+    def is_ok_and(self, f: Callable[[T], bool]) -> bool:
+        match self:
+            case Result.Ok(value): return f(value)
+            case Result.Err(_): return False
+            case _: unreachable()
+
+    def is_err(self) -> bool:
+        return self == Result.Err(_)
+    
+    def is_err_and(self, f: Callable[[T], bool]) -> bool:
+        match self:
+            case Result.Ok(_): return False
+            case Result.Err(err): return f(err)
+            case _: unreachable()
+
     def ok(self) -> Option[T]:
         match self:
             case Result.Ok(value): return Option.Some(value)
-            case Result.Err(): return Option.Not()
+            case Result.Err(_): return Option.Not()
             case _: unreachable()
 
-    def err(self) -> Option[T]:
+    def err(self) -> Option[E]:
         match self:
-            case Result.Ok(value): return Option.Not()
+            case Result.Ok(_): return Option.Not()
             case Result.Err(err): return Option.Some(err)
+            case _: unreachable()
+
+    # def as_ref(self): pass
+    # def as_mut(self): pass
+
+    def map(self, op: Callable[[T], U]) -> Self:
+        match self:
+            case Result.Ok(value): return Result.Ok(op(value))
+            case Result.Err(_): return self
+            case _: unreachable()
+
+    def map_or(self, default: U, f: Callable[[T], U]) -> U:
+        match self:
+            case Result.Ok(value): return f(value)
+            case Result.Err(_): return default 
+            case _: unreachable()
+
+    # def map_or_else(self): pass
+    # def map_err(self): pass
+
+    def map_err(self, op: Callable[[E], F]) -> Self:
+        match self:
+            case Result.Ok(_): return self
+            case Result.Err(err): return Result.Err(op(err))
+            case _: unreachable()
+
+    # def inspect(self): pass
+    # def inspect_err(self): pass
+    # def as_deref(self): pass
+    # def as_deref_mut(self): pass
+    # def iter(self): pass
+    # def iter_mut(self): pass
+
+    def expect(self, msg: str) -> T:
+        match self:
+            case Result.Ok(value): return value
+            case Result.Err(err): raise Exception(f"{msg}: {err}")
             case _: unreachable()
 
     def unwrap(self) -> T:
@@ -30,32 +87,72 @@ class Result[T, E]:
             case Result.Err(err): raise Exception(f"called `Result.unwrap()` on a `Not` value: \"{err}\"")
             case _: unreachable()
 
+    """
+    def unwrap_or_default(self) -> T:
+        match self:
+            case Result.Ok(value): return value
+            case Result.Err(_): return type(T)()
+            case _: unreachable()
+    """
+
+    def expect_err(self, msg: str) -> T:
+        match self:
+            case Result.Ok(value): raise Exception(f"{msg}: {value}")
+            case Result.Err(err): return err
+            case _: unreachable()
+
+    def unwrap_err(self) -> E:
+        match self:
+            case Result.Ok(value): raise Exception(value)
+            case Result.Err(err): return err
+            case _: unreachable()
+
+    def andd(self, res: Self) -> Self:
+        match self:
+            case Result.Ok(_): return res
+            case Result.Err(_): return self
+            case _: unreachable()
+
+    def and_then(self, op: Callable[[T], Self]) -> Self:
+        match self:
+            case Result.Ok(value): return op(value)
+            case Result.Err(_): return self
+            case _: unreachable()
+
+    def orr(self, res: Self) -> Self:
+        match self:
+            case Result.Ok(_): return self 
+            case Result.Err(_): return res
+            case _: unreachable()
+
+    def or_else(self, op: Callable[[E], Self]) -> Self:
+        match self:
+            case Result.Ok(_): return self
+            case Result.Err(err): return op(err)
+            case _: unreachable()
+
+
     def unwrap_or(self, default: T) -> T:
         match self:
             case Result.Ok(value): return value
-            case Result.Err(): return default 
+            case Result.Err(_): return default 
             case _: unreachable()
 
     def unwrap_or_else(self, f: Callable[[...], T]) -> T:
         match self:
             case Result.Ok(value): return value
-            case Result.Err(): return f() 
+            case Result.Err(_): return f() 
             case _: unreachable()
 
-    """
-    def unwrap_or_default(self) -> T:
-        match self:
-            case Result.Ok(value): return value
-            case Result.Err(): return type(T)()
-            case _: unreachable()
-    """
+    # def unwrap_unchecked(self): pass
+    # def unwrap_err_unchecked(self): pass
 
-def result_convert(f: Callable[[...], T]) -> Callable[[...], Result[T, E]]:
-    def wrapper(*args, **kwargs) -> Result[T, E]:
-        try:
-            res = f(*args, **kwargs)
-            return Result.Ok(res)
-        except Exception as e:
-            return Result.Err(str(e))
+    def convert(f: Callable[[...], T]) -> Callable[[...], Self]:
+        def wrapper(*args, **kwargs) -> Result[T, E]:
+            try:
+                res = f(*args, **kwargs)
+                return Result.Ok(res)
+            except Exception as e:
+                return Result.Err(str(e))
 
-    return wrapper
+        return wrapper

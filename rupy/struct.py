62ef6f05e1ac9b87a330b84c8d136e00b6451264
Endre Bjørgo <endre.bjoergo@tryg.no>
2025-01-22 12:57:35 +0100

    Fix impl decorator

--- rupy/struct.py
+++ rupy/struct.py
@@ -1,26 +1,30 @@
 import sys
 from typing import Callable, Type
 from dataclasses import dataclass
+from abc import ABCMeta
 
 class StructImplMeta(type):
     def __new__(cls, name, bases, dct):
         module_name = dct.get('__module__')
         module_globals = sys.modules[module_name].__dict__
         if name in module_globals:
-            if "__annotations__" in dct:
-                new_annotations = [x for x in dct["__annotations__"]]
-                raise Exception(f"Cannot add struct fields more than once: {new_annotations}")
+            if annotations := dct.get("__annotations__"):
+                new_annotations = [x for x in annotations]
+                if new_annotations:
+                    raise Exception(f"Cannot add struct fields more than once: {new_annotations}")
 
             existing_cls = module_globals[name]
             for key, value in dct.items():
                 if key not in ('__module__', '__qualname__', '__dict__'):
-                    if not hasattr(existing_cls, key): print(key)
                     setattr(existing_cls, key, value)
             return existing_cls
         else:
             return super().__new__(cls, name, bases, dct)
 
-class StructBase(metaclass=StructImplMeta):
+class StructBaseMeta(StructImplMeta, ABCMeta):
+    pass
+
+class StructBase(metaclass=StructBaseMeta):
     pass
 
 def struct(cls):
@@ -29,8 +33,24 @@ def struct(cls):
 
 def impl(trait: Type) -> Callable[[Type], Type]:
     def decorator(cls):
-        new_cls = struct(cls)
-        return new_cls
+        # TODO: Replace the following with ABC functionality
+        a = [
+            key for key, val in trait.__dict__.items() 
+            if callable(val) and not key.startswith('__')
+        ]
+        b = [
+            key for key, val in cls.__dict__.items() 
+            if callable(val) and not key.startswith('__')
+        ]
+        if set(a) != set(b):
+            raise Exception(f"Implementation of trait `{trait.__name__}` for `{cls.__name__}` does not contain correct methods")
+
+        new_cls = type(
+            cls.__name__,
+            (trait, cls),
+            dict(cls.__dict__)
+        )
+        return struct(new_cls)
 
     return decorator
 

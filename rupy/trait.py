b0ba4ab41518ad9ed7bfd99e5e1b059a85470803
Endre Bjørgo <endre.bjoergo@tryg.no>
2025-01-24 11:18:23 +0100

    Add trait definition validation

--- rupy/trait.py
+++ rupy/trait.py
@@ -6,16 +6,16 @@ from abc import ABC, abstractmethod, ABCMeta
 
 
 def trait(cls):
-    # TODO: Assert that there are only methods
-
     for key, value in cls.__dict__.items():
-        if not callable(value) or key.startswith('__'): continue
+        if key.startswith('__'): continue
+        if not callable(value):
+            raise Exception(f"Trait definition may not contain attributes: {cls.__name__}")
 
         source = textwrap.dedent(inspect.getsource(value))
         tree = ast.parse(source)
 
         if len(tree.body) != 1 or not isinstance(tree.body[0].body[0], ast.Pass):
-            raise Exception(f"Definition of trait {cls.__name__} contains erronous method")
+            raise Exception(f"Trait method definition may only contain pass: {cls.__name__}")
 
     return cls
 
